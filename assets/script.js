a48b232dc03e82d81f2c69ab8b8550420dde69a0
Emil Cioran <john.a.alzate@gmail.com>
2024-01-07 04:46:11 -0500

    Add timestamp for sample row

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,9 @@
 const currentDay = dayjs().format('MM DD YYYY');
 $('#todaysDate').text(currentDay);
+
+const saveBtn = document.getElementById('save-btn');
+
+saveBtn.addEventListener('click', function () {
+    //Code to save data goes here
+    console.log('Data saved!');
+});
