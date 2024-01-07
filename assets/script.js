096e47b5e0c7dd37f570dda89d069e6f66509339
Emil Cioran <john.a.alzate@gmail.com>
2024-01-07 06:35:40 -0500

    Begin creating if loop and create 3 + 1 variables (rowColor and present/past/future)using getElementsByClassName

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,34 @@
 const currentDay = dayjs().format('MM DD YYYY');
-$('#todaysDate').text(currentDay);
 
-const saveBtn = document.getElementById('save-btn');
+
+$('#todaysDate').text(currentDay);
+const saveBtn = document.getElementByClassName('save-btn');
 
 saveBtn.addEventListener('click', function () {
     //Code to save data goes here
     console.log('Data saved!');
 });
+
+const currentTime = dayjs().format('HH');
+
+const t = currentTime;
+
+const rowColor = document.getElementsByClassName("row").style.backgroundColor;
+
+const present = document.getElementsByClassName("row").style.backgroundColor;
+const past = document.getElementsByClassName("row").style.backgroundColor;
+const future = document.getElementsByClassName("row").style.backgroundColor;
+
+function changeColor () {
+    if (t < 12) {
+        rowColor = present;
+    } else if (t < 20) {
+        rowColor = past;
+    } else {
+        rowColor = future;
+    }   
+}
+
+//const changeColor = document.getElementById('textarea');
+
+//textarea.addEventListener()
